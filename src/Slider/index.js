f63112e6d6ecc8b9b690085af210ff172a6f6b53
lovecolor <69668572+lovecolor@users.noreply.github.com>
2021-01-30 18:18:13 +0700

    Update index.js

--- src/Slider/index.js
+++ src/Slider/index.js
@@ -1,9 +1,5 @@
 import './style.css'
 import React, { Component } from 'react'
-import img1  from "./img1.jpg";
-import img2  from "./img2.jpg";
-import img3  from "./img3.jpg";
-import img4  from "./img4.jpg";
 export default class index extends Component {
     constructor(props){
         super(props)
