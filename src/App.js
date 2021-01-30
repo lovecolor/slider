c8bf61e781c1438ca3406036ec19cc96e1e63458
Your Name <hoanglong0507000@gmail.co$ git config --global user.email hoanglong0507000@gmail.co>
2021-01-30 17:57:50 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css';
+import Slider from './Slider';
+
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <Slider/>
+      </div>
+    )
+  }
+}
---
+++ src/Slider/index.js
@@ -0,0 +1,53 @@
+import './style.css'
+import React, { Component } from 'react'
+import img1  from "./img1.jpg";
+import img2  from "./img2.jpg";
+import img3  from "./img3.jpg";
+import img4  from "./img4.jpg";
+export default class index extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            color:0,
+            arrColor:['https://cdn.mobilecity.vn/mobilecity-vn/images/2020/05/man-hinh-xiaomi-redmi-k30.jpg','https://cdn.mobilecity.vn/mobilecity-vn/images/2020/05/nut-nguon-van-tay-xiaomi-redmi-k30.jpg','https://cdn.mobilecity.vn/mobilecity-vn/images/2020/05/canh-duoi-xiaomi-redmi-k30.jpg','https://cdn.mobilecity.vn/mobilecity-vn/images/2020/05/cam-truoc-xiaomi-redmi-k30.jpg']
+        }
+    }
+    nextImg=()=>{
+        this.setState(
+            {
+                color:this.state.color===3?0:this.state.color+1
+            }
+        )
+    }
+    preImg=()=>{
+        this.setState(
+            {
+                color:this.state.color===0?3:this.state.color-1
+            }
+        )
+    }
+    componentDidMount()
+    {
+        
+        setInterval(() => {
+            this.nextImg()
+        }, 1000);
+            
+        
+    }
+    render() {
+        const {arrColor}=this.state;
+            const {color}=this.state;
+        return (
+            
+            <div>
+                <img className='slider' src={arrColor[color]} >
+                
+            </img>
+            <button className='btn-previos' onClick={()=>this.preImg()}>&#60;</button>
+                <button className='btn-next' onClick={()=>this.nextImg()}>&#62;</button>
+            </div>
+            
+        )
+    }
+}
